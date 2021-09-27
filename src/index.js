7a1da548051d6ddbf1cddb2c2a4f1560e7e64d80
Lupita-G <lupitagm390@gmail.com>
2021-09-27 17:54:31 -0500

    added Hello World

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello from my library app');
+    res.send('Hello World!');
 })
 
 app.listen(3000, function(){
