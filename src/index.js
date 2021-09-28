f42e53ead36968183f2be5566597bc994831c9a7
Lupita-G <lupitagm390@gmail.com>
2021-09-28 09:11:54 -0500

    modificando peticion

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const express = require ('express');
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    res.send('');
-    
-})
+app.get('/', function (req, res){
+    res.send('Hello World'); 
+
+});
+
+app.get('/users', (req, res) =>{
+    res.send('[]');
+});
+
+app.get('/users', )
 app.listen(3000, function(){
     console.log('listening on port 3000');
 })
