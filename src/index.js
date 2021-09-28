e2a6cc9a74dac52776bca6ac3b88e1b3a427e73b
Lupita-G <lupitagm390@gmail.com>
2021-09-28 14:04:13 -0500

    finish the level

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 const express = require ('express');
+
 const app = express();
+
+let users = [];
 
 app.get('/', function (req, res){
     res.send('Hello World!'); 
 
 });
 
-app.get('/users', (req, res)=> {
-    res.send([]);
+
+app.get('/users', (req, res)=>{
+    res.send(users.map((item) => (
+        {id: item}
+    )));
 });
 
-app.post('/user', function(req, res) {
-    res.send({ id: 0});
+
+app.post('/user', function(req, res){
+    users.push(users.length);
+
+    res.status(200).json({id: users[users.length - 1]})
 });
 
-app.get('/users', function(req, res){
-    res.send([{ id: 0}]);
+app.get('/user/:id', function(req, res){
+    res.send({ id: Number(req.params.id)});
 });
+ 
 
-app.get('/user/0', function(req, res){
-    res.send({ id: 0});
-});
 
 app.listen(3000, function(){
     console.log('listening on port 3000');
